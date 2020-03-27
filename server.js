c6c584570c65fe11bc15a36cf7ef9859a1d1cece
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2020-03-27 21:30:37 +1100

    made app a PWA

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express=require("express"),logger=require("morgan"),mongoose=require("mongoose"),compression=require("compression"),PORT=process.env.PORT||3e3,app=express();app.use(logger("dev")),app.use(compression()),app.use(express.urlencoded({extended:!0})),app.use(express.json()),app.use(express.static("public")),mongoose.connect("mongodb://localhost/budget",{useNewUrlParser:!0,useFindAndModify:!1}),app.use(require("./routes/api.js")),app.listen(PORT,()=>{console.log(`App running on port ${PORT}!`)});
+var compression = require('compression')
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// compress all responses
+app.use(compression())
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
